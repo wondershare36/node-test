921d768e6331d07c69aada91d90e99f31073829a
wusili <498282023@qq.com>
2021-02-17 15:44:04 +0800

    can fs.readfile

--- server.js
+++ server.js
@@ -1,47 +1,46 @@
-const http=require("http")
-const fs=require("fs")
-const url=require("url")
-const port =process.argv[2]
-
-if(!port){
-  console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+const port = process.argv[2]
+if (!port) {
+  console.log("请输入端口参数：node server.js 端口");
   process.exit(1)
 }
-
-var server=http.createServer((req,res)=>{
-  var parseURL=url.parse(req.url,true)
-  var pathWithQuery=req.url
-  var queryString=""
-  if(pathWithQuery.indexOf("?")>=0){
-    queryString=pathWithQuery.substring(pathWithQuery.indexOf("?"))
+const server = http.createServer((req, res) => {
+  var parseUrl = url.parse(req.url)
+  var { path: pathWithQuery, query, pathname } = parseUrl
+  var method = req.method
+  console.log(`收到请求！路径（带查询参数）为：${pathWithQuery}`);
+  path.join(__dirname, "/static/", pathname)
+  if (pathname === "/") {
+    fs.readFile("static/index.html", "utf-8", (err, data) => {
+      if (err) {
+        console.log(err);
+        res.statusCode = 404
+      } else {
+        res.write(data)
+        res.end()
+      }
+    })
   }
-  var path=parseURL.pathname
-  var query=parseURL.query
-  var method=req.method
-  /******** 从这里开始看，上面不要看 ************/
-
-  console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
-
-  if(path === '/'){
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/html;charset=utf-8')
-    res.write(`哈哈哈`)
-    res.end()
-  } else if(path === '/x'){
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/css;charset=utf-8')
-    res.write(`body{color: red;}`)
-    res.end()
-  } else {
-    res.statusCode = 404
-    res.setHeader('Content-Type', 'text/html;charset=utf-8')
-    res.write(`你输入的路径不存在对应的内容`)
-    res.end()
+  else if (pathname.indexOf("static")) {
+    try {
+      fs.readFile(path.join(__dirname, "/static/", pathname), "utf-8", (err, data) => {
+        if (err) {
+          console.log(err);
+          res.statusCode = 404
+        } else {
+          res.write(data)
+          res.end()
+        }
+      })
+    } catch (error) {
+      
+    }
   }
 
-  /******** 代码结束，下面不要看 ************/
 })
 
-
 server.listen(port)
-console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
+console.log("服务运行在 http://localhost:" + port);
